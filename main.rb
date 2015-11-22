711193f57777b4f145ccffb18af91ce046a34afb
shts <shts.dev@gmail.com>
2015-11-22 16:58:39 +0900

    fix for blog

--- main.rb
+++ main.rb
@@ -10,13 +10,15 @@ require 'parse-ruby-client'
 # TODO: Windowsのみで発生する証明書問題によりSSL認証エラーの暫定回避策
 #ENV['SSL_CERT_FILE'] = File.expand_path('C:\rumix\ruby\2.1\i386-mingw32\lib\ruby\2.1.0\rubygems\ssl_certs\cert.pem')
 
-# Parseライブラリの初期化
 Parse.init :application_id => ENV['PARSE_APP_ID'],
            :api_key        => ENV['PARSE_API_KEY']
 
 BaseUrl = "http://www.keyakizaka46.com"
-def fetch
+# http://blog.keyakizaka46.com/mob/news/diarKiji.php?site=k46&ima=2653&cd=member&ct=01
+BaseBlogUrl = "http://blog.keyakizaka46.com/mob/news/diarKiji.php?site=k46&ima=2653&cd=member&ct="
+ParseClassName = "Member"
 
+def fetch
   doc = Nokogiri::HTML(open(BaseUrl))
   doc.css('div#member').css('div.popup_win').each do |member|
     data = { :name_sub => nil,
@@ -47,14 +49,28 @@ def fetch
     end
     yield(data) if block_given?
   end
-
 end
 
+def to_blog_url(member_counter)
+  if member_counter < 10
+    return "#{BaseBlogUrl}0#{member_counter}"
+  else
+    return "#{BaseBlogUrl}#{member_counter}"
+  end
+end
+
+member_counter = 0
 fetch { |data|
-  query = Parse::Query.new("Member").eq("image_url", data[:image_url])
-  member = query.get.first
+  member_counter = member_counter + 1
+  # 辞退したメンバーのidはスキップする
+  if member_counter == 16
+    member_counter = member_counter + 1
+  end
+
+  member = Parse::Query.new(ParseClassName).eq("image_url", data[:image_url]).get.first
   if member == nil then
-    new_member = Parse::Object.new("Member")
+    new_member = Parse::Object.new(ParseClassName)
+    data[:blog_url] = to_blog_url(member_counter)
     data.each { |key, val|
       new_member[key] = val
     }
